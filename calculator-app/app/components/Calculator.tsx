aecae377d034c62ea0de1106ea467403791f82c1
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2024-06-20 18:10:39 -0700

    add display component

--- calculator-app/app/components/Calculator.tsx
+++ calculator-app/app/components/Calculator.tsx
@@ -1,7 +1,27 @@
-export default function Calculator(){
-    return(
-        <div className="w-1/6 h-1/2 opacity-50 bg-gradient-to-b from-gray-100 to-gray-400 rounded shadow-2xl">
+import { useState } from 'react';
 
+export default function Calculator(){
+    const [result, setResult] = useState('');
+    const [history, setHistory] = useState('');
+
+    return(
+        <div className="w-1/6 h-1/2 bg-gradient-to-b from-gray-100 to-gray-400 rounded shadow-2xl opacity-50 ">
+        {/* Display */}
+        <Display result={result} history={history}/>
+
+        {/* Buttons */}
         </div>
     );
+}
+
+function Display({result, history}:{result:string, history:string}){
+  return (
+      <div>
+        {/* histories */}
+        <p>{history}</p>
+
+        {/* output */}
+        <p>{result}</p>
+      </div>
+  );
 }
