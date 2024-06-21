bd7b0aca819ae61c86cf1c908d09dd35ddc38e39
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2024-06-20 20:51:45 -0700

    styled display

--- calculator-app/app/components/Calculator.tsx
+++ calculator-app/app/components/Calculator.tsx
@@ -1,27 +1,52 @@
+'use client';
+
 import { useState } from 'react';
 
 export default function Calculator(){
-    const [result, setResult] = useState('');
-    const [history, setHistory] = useState('');
+    const [history, setHistory] = useState('0');
+    const [result, setResult] = useState('0');
 
     return(
-        <div className="w-1/6 h-1/2 bg-gradient-to-b from-gray-100 to-gray-400 rounded shadow-2xl opacity-50 ">
-        {/* Display */}
-        <Display result={result} history={history}/>
+        <div className="w-80 h-1/2 bg-gradient-to-b from-gray-100 to-gray-400 rounded-2xl shadow-2xl opacity-50">
+            <Display result={result} history={history}/>
 
-        {/* Buttons */}
+            {/* Buttons */}
+            <div>
+                <Button symbol="Ac"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+                <Button symbol="0"></Button>
+            </div>
         </div>
     );
 }
 
 function Display({result, history}:{result:string, history:string}){
   return (
-      <div>
+      <div className="w-full h-1/3 p-5 rounded-3xl flex flex-col items-end justify-end">
         {/* histories */}
-        <p>{history}</p>
+        <p className="text-gray-700 text-3xl">{history}</p>
 
         {/* output */}
-        <p>{result}</p>
+        <p className="text-black font-bold text-4xl">{result}</p>
       </div>
   );
+}
+
+function Button({symbol}:{symbol:string}){
+    return(
+        <button>{symbol}</button>
+    );
 }
