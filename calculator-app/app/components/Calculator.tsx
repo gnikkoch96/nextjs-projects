329f0b0c19ee456545b23e948e00b17cb203c1de
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2024-06-21 11:49:03 -0700

    updated styles

--- calculator-app/app/components/Calculator.tsx
+++ calculator-app/app/components/Calculator.tsx
@@ -7,27 +7,29 @@ export default function Calculator(){
     const [result, setResult] = useState('0');
 
     return(
-        <div className="w-80 h-1/2 bg-gradient-to-b from-gray-100 to-gray-400 rounded-2xl shadow-2xl opacity-50">
+        <div className="w-80 h-1/2 p-5 bg-gradient-to-b from-gray-100 to-gray-400 rounded-2xl shadow-2xl opacity-50">
             <Display result={result} history={history}/>
 
             {/* Buttons */}
-            <div>
+            <div className="h-2/3 grid grid-cols-4 gap-0.5 justify-items-center">
                 <Button symbol="Ac"></Button>
+                <Button symbol="<x"></Button>
+                <Button symbol="/"></Button>
+                <Button symbol="*"></Button>
+                <Button symbol="7"></Button>
+                <Button symbol="8"></Button>
+                <Button symbol="9"></Button>
+                <Button symbol="-"></Button>
+                <Button symbol="4"></Button>
+                <Button symbol="5"></Button>
+                <Button symbol="6"></Button>
+                <Button symbol="+"></Button>
+                <Button symbol="1"></Button>
+                <Button symbol="2"></Button>
+                <Button symbol="3"></Button>
                 <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
-                <Button symbol="0"></Button>
+                <Button symbol="."></Button>
+                <Button symbol="="></Button>
             </div>
         </div>
     );
@@ -35,7 +37,7 @@ export default function Calculator(){
 
 function Display({result, history}:{result:string, history:string}){
   return (
-      <div className="w-full h-1/3 p-5 rounded-3xl flex flex-col items-end justify-end">
+      <div className="w-full h-1/3 pb-5 rounded-3xl flex flex-col items-end justify-end">
         {/* histories */}
         <p className="text-gray-700 text-3xl">{history}</p>
 
@@ -47,6 +49,6 @@ function Display({result, history}:{result:string, history:string}){
 
 function Button({symbol}:{symbol:string}){
     return(
-        <button>{symbol}</button>
+        <button className="w-11/12 rounded-2xl bg-amber-700">{symbol}</button>
     );
 }
