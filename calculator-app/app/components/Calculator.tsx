242c017c999ff58988565be8874bfb33e85f46b7
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2024-06-24 15:55:09 -0700

    updated styles

--- calculator-app/app/components/Calculator.tsx
+++ calculator-app/app/components/Calculator.tsx
@@ -9,6 +9,30 @@ export default function Calculator(){
     const [history, setHistory] = useState('0');
     const [result, setResult] = useState('0');
 
+    function handleButton(symbol: string) {
+        if (symbol === 'Ac') {
+            // clear history and result
+            setHistory('0');
+            setResult('0');
+        } else if (symbol === '<x') {
+            // delete from the display or reset to 0 if there is no more numbers
+            if (result.length == 1) setResult('0');
+            else setResult(result.substring(0, result.length - 1));
+        } else if (symbol === '=') {
+
+        } else if (symbol === '.') {
+            if(result.match('[.]')) return;
+            setResult(result + symbol);
+        } else { // number
+            if(result === '0'){
+                setResult(symbol);
+            }else{
+                // add number to display
+                setResult(result + symbol);
+            }
+        }
+    }
+
     return(
         <div className="w-[360px] h-[640px] p-5 bg-gradient-to-br from-blue-50 to-blue-300 rounded-2xl shadow-2xl">
             <Display result={result} history={history}/>
@@ -16,7 +40,7 @@ export default function Calculator(){
             {/* Buttons */}
             <div className="h-2/3 grid grid-cols-4 gap-3 justify-items-center">
                 {symbols.map((symbol) => (
-                    <Button key={symbol} symbol={symbol}/>
+                    <Button key={symbol} symbol={symbol} handleClick={() => handleButton(symbol)}/>
                 ))}
             </div>
         </div>
@@ -35,21 +59,22 @@ function Display({result, history}:{result:string, history:string}){
   );
 }
 
-function Button({symbol}:{symbol:string}){
+function Button({symbol, handleClick}:{symbol:string, handleClick: () => void}){
     // test to see if the symbol is a number
     const numRegex = new RegExp("[0-9]")
 
     return(
         <button
             className={clsx(
-                "w-11/12 rounded-2xl border border-white font-bold shadow text-3xl",
+                "w-11/12 rounded-2xl border border-white font-bold shadow text-3xl hover:opacity-50 transition",
             {
                 "text-blue-400": !numRegex.test(symbol),
-                'opacity-50 shadow-2xl': symbol === '*' || symbol === '-' || symbol === '+' || symbol === '/',
+                'opacity-70 shadow-2xl': symbol === '*' || symbol === '-' || symbol === '+' || symbol === '/',
                 "text-white": numRegex.test(symbol) || symbol === '.' || symbol === '=',
                 'col-span-2': symbol ===  '0',
-                'row-span-2': symbol === '=',
+                'bg-sky-400 row-span-2': symbol === '=',
             })}
+            onClick={handleClick}
         >
             {symbol}
         </button>
