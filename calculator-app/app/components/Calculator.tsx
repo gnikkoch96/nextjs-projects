d0bdb8c50ef73a3febb0fdaebbb2c9e4b7a429df
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2024-07-04 09:12:05 -0700

    added Ac logic

--- calculator-app/app/components/Calculator.tsx
+++ calculator-app/app/components/Calculator.tsx
@@ -6,16 +6,16 @@ import {Simulate} from "react-dom/test-utils";
 
 export default function Calculator(){
     // math expression
-    const [history, setHistory] = useState('0');
+    const [expression, setExpression] = useState('');
 
     // display result
-    const [result, setResult] = useState('0');
+    const [display, setDisplay] = useState('0');
 
     // stores previous number entered before operator press (first operand)
-    const [previousValue, setPreviousValue] = useState('0');
+    const [previousValue, setPreviousValue] = useState('' || null);
 
     // stores the most recent operator
-    const [currentOperator, setCurrentOperator] = useState('0');
+    const [currentOperator, setCurrentOperator] = useState('' || null);
 
     // flag used to indicate if next digit should start a new number or append to current number
     const [isNewInput, setIsNewInput] = useState(false);
@@ -29,16 +29,21 @@ export default function Calculator(){
         switch(symbol){
             case "Ac":
                 // clear displays
-                setResult('0');
-                setHistory('0');
+                setDisplay('0');
+                setExpression('');
 
+                // reset state vars
+                setPreviousValue(null);
+                setCurrentOperator(null);
+                setIsNewInput(false);
+                setHasDecimal(false);
                 break;
         }
     }
 
     return(
         <div className="w-[360px] h-[640px] p-5 bg-gradient-to-br from-blue-50 to-blue-300 rounded-2xl shadow-2xl">
-            <Display result={result} history={history}/>
+            <Display display={display} expression={expression}/>
 
             {/* Buttons */}
             <div className="h-2/3 grid grid-cols-4 gap-3 justify-items-center">
@@ -50,14 +55,14 @@ export default function Calculator(){
     );
 }
 
-function Display({result, history}:{result:string, history:string}){
+function Display({display, expression}:{display:string, expression:string}){
   return (
       <div className="w-full h-1/3 pb-5 rounded-3xl flex flex-col items-end justify-end">
         {/* histories */}
-        <p className="text-gray-400 text">{history}</p>
+        <p className="text-gray-400 text">{expression}</p>
 
         {/* output */}
-        <p className="text-gray-700 font-bold text-4xl">{result}</p>
+        <p className="text-gray-700 font-bold text-4xl">{display}</p>
       </div>
   );
 }
