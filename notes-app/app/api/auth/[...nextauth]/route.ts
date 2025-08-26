f0c2b038a798cb2dc0e43e57979103a17678d07b
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2025-08-25 18:22:23 -0700

    add: nextauth config

---
+++ notes-app/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "Enter your email" },
+                password: { label: "Password", type: "password", placeholder: "Enter your password" }
+            },
+
+            async authorize(credentials, req) {
+                const user = { id: "1", name: "User", email: "user@example.com" };
+                
+                if(user.email === credentials?.email && credentials?.password === "password") {
+                    return user;
+                }else{
+                    return null;
+                }
+            }
+        })
+    ]
+
+}
+
+export default NextAuth(authOptions);
