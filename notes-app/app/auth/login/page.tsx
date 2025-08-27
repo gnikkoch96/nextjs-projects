377a4358ba2e0bcb148064df383834d442f51a1c
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2025-08-27 12:04:30 -0700

    style: add logo and centered login page

--- notes-app/app/auth/login/page.tsx
+++ notes-app/app/auth/login/page.tsx
@@ -1,15 +1,21 @@
 "use client";
 
 import { Container, Box, TextField, Button } from "@mui/material";
+import DescriptionRoundedIcon from "@mui/icons-material/DescriptionRounded";
 
 export default function Login() {
   return (
-    <Container maxWidth="sm">
-      <Box 
-        component="form">
-            <TextField label="Email" type="email"/>
-            <TextField label="Password" type="password"/>
-            <Button type="submit" variant="contained">Sign In</Button>
+    <Container fixed className="h-full flex justify-center items-center">
+      <Box component="form" className="flex flex-col gap-2 w-1/2">
+        <span className="flex flex-col items-center gap-2">
+          <DescriptionRoundedIcon fontSize="large" />
+          Nikko's Note App
+        </span>
+        <TextField label="Email" type="email" />
+        <TextField label="Password" type="password" />
+        <Button type="submit" variant="contained">
+          Sign In
+        </Button>
       </Box>
     </Container>
   );
