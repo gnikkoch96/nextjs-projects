da3866bfcf14c1cf63bb568bc7dd665505b1b5b8
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2025-08-27 19:48:24 -0700

    feat: added a signup page

--- notes-app/app/auth/login/page.tsx
+++ notes-app/app/auth/login/page.tsx
@@ -2,11 +2,12 @@
 
 import { Container, Box, TextField, Button } from "@mui/material";
 import DescriptionRoundedIcon from "@mui/icons-material/DescriptionRounded";
+import Link from "next/link";
 
 export default function Login() {
   return (
     <Container fixed className="h-full flex justify-center items-center">
-      <Box component="form" className="flex flex-col gap-2 w-1/2">
+      <Box component="form" className="flex flex-col gap-2 xs:w-1/2">
         <span className="flex flex-col items-center gap-2">
           <DescriptionRoundedIcon fontSize="large" />
           Nikko's Note App
@@ -16,6 +17,10 @@ export default function Login() {
         <Button type="submit" variant="contained" color="primary">
           Sign In
         </Button>
+        <span className="text-center mt-3">
+          Don't have an account?{" "}
+          <Link href="/auth/signup" className="underline pointer hover:opacity-75">Create one here!</Link>
+        </span>
       </Box>
     </Container>
   );
--- notes-app/app/auth/signup/page.tsx
+++ notes-app/app/auth/signup/page.tsx
@@ -1,3 +1,31 @@
-export default function Signup(){
-    return (<div>This is a signup page</div>);
+import DescriptionRoundedIcon from "@mui/icons-material/DescriptionRounded";
+import { Container, Box, TextField, Button } from "@mui/material";
+import Link from "next/link";
+
+export default function Signup() {
+  return (
+    <Container fixed className="h-full flex justify-center items-center">
+      <Box component="form" className="flex flex-col gap-2 xs:w-1/2">
+        <span className="flex flex-col items-center gap-2">
+          <DescriptionRoundedIcon fontSize="large" />
+          Nikko's Note App
+        </span>
+        <TextField label="Email" type="email" />
+        <TextField label="Password" type="password" />
+        <TextField label="Re-Type Password" type="password" />
+        <Button type="submit" variant="contained" color="primary">
+          Sign In
+        </Button>
+        <span className="text-center mt-3">
+          Already have an account?
+          <Link
+            href="/auth/login"
+            className="underline pointer hover:opacity-75"
+          >
+            Sign in here!
+          </Link>
+        </span>
+      </Box>
+    </Container>
+  );
 }
