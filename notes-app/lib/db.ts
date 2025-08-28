1a9e2e0cae27282db7bebdcb5c708715ef9aa191
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2025-08-27 20:03:38 -0700

    feat: create a pool connection to mysql database

---
+++ notes-app/lib/db.ts
@@ -0,0 +1,10 @@
+import mysql from 'mysql2/promise';
+
+// This file will contain the helper functions used to interact with the MySQL database
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
